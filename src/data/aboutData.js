c291c4854b3c666d7d012e8bdf60a7204b2609ac
IswaryaRavikumar415 <iswaryaravikumar415@gmail.com>
2022-06-27 14:19:20 +0530

    second commit

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,6 +1,6 @@
 export const aboutData = {
     title: "Who I am",
     description1: "My name is Iswarya Ravikumar from cotton city Tirupur. I am a UX UI Designer and Frontend Developer.",
-    description2: " I have completed MCA at RVS College of Arts and science, Sullur. Also while doing MCA, I completed my internship. During internship I worked in UX UI Designing and Frontend Development part of live projects for my college. Right now I'm looking for a carrier opportunity to fully utilize my skills. I love to learn and explore new areas.",
+    description2: " I have completed MCA at RVS College of Arts and science, Sulur. Also while doing MCA, I completed my internship. During internship I worked in UX UI Designing and Frontend Development part of live projects for my college. Right now I'm looking for a carrier opportunity to fully utilize my skills. I love to learn and explore new areas.",
     image: 2
 }
--- src/data/headerData.js
+++ src/data/headerData.js
@@ -1,4 +1,5 @@
-import resume from '../assets/pdf/Iswarya Ravikumar MCA.pdf';
+import resume1 from '../assets/pdf/Iswarya Ravikumar MCA.pdf';
+import resume from '../assets/pdf/Iswarya Resume MCA.pdf';
 //import avatar from '../../public/img1.jpeg';
 
 export const headerData = {
